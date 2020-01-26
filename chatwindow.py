22b43f883127f96d1a42557d1b93b38ff0224000
HuaxinZhang <dianazhang0219@gmail.com>
2020-01-26 00:27:26 -0700

    chat

---
+++ chatwindow.py
@@ -0,0 +1,142 @@
+import os
+import sys
+#import server
+import client
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.clock import Clock
+
+# ----------------- SIGN IN PAGE ----------------- #
+class ConnectPage(GridLayout):
+    def __init__(self, **kwargs):
+        super(ConnectPage, self).__init__(**kwargs)
+        self.cols = 2
+
+        if os.path.isfile("prev_details.txt"):
+            with open("prev_details.txt", "r") as f:
+                d = f.read().split(",")
+                prev_ip = d[0]
+                prev_port = d[1]
+                prev_username = d[2]
+
+        else:
+            prev_ip = ""
+            prev_port = ""
+            prev_username = ""
+
+        self.add_widget(Label(text="IP: "))
+        self.ip = TextInput(text = prev_ip, multiline=False)
+        self.add_widget(self.ip)
+
+        self.add_widget(Label(text="PORT: "))
+        self.port = TextInput(text = prev_port, multiline=False)
+        self.add_widget(self.port)
+
+        self.add_widget(Label(text = "Username: "))
+        self.username = TextInput(text = prev_username, multiline = False)
+        self.add_widget(self.username)
+
+        self.join = Button(text="Join")
+        self.add_widget(Label())
+        self.join.bind(on_press=self.join_button)
+        self.add_widget(self.join)
+
+    def join_button(self, instance):
+        port = self.port.text
+        ip = self.ip.text
+        username = self.username.text
+
+        with open("prev_details.txt", "w") as f:
+            f.write(f"{ip},{port},{username}")
+
+        info = f"Attemping to join {ip}:{port} as {username}"
+        chat_app.info_page.update_info(info)
+
+        # ------------ CHANGE PAGE ------------ #
+        chat_app.screen_manager.current = "Info"
+        Clock.schedule_once(self.connect, 1)
+
+    def connect(self, _):
+        port = int(self.port.text)
+        ip = self.ip.text
+        username = self.username.text
+
+        if not client.create_client_socket(ip, port, username, show_error):
+            return
+
+        print("CHANGE TO CHAT")
+        # since chat page listen to connection
+        # so we have to create chatpage after connection
+        chat_app.create_chat_page()
+        chat_app.screen_manager.current = "Chat"
+
+
+
+# ----------------- INFO PAGE ----------------- #
+class InfoPage(GridLayout):
+    def __init__(self, **kwargs):
+        super(InfoPage, self).__init__(**kwargs)
+        self.cols = 1
+        self.message = Label(halign="center", valign="middle", font_size=30)
+        self.message.bind(width= self.update_text_width)
+        self.add_widget(self.message)
+
+    def update_info(self, message):
+        self.message.text = message
+
+    def update_text_width(self, *_):
+        self.message.text_size = (self.message.width*0.9, None)
+
+
+# ----------------- CHAT PAGE ----------------- #
+class ChatPage(GridLayout):
+    def __init__(self, **kwargs):
+        super(ChatPage, self).__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(Label(text = "Begin Chat !!"))
+
+
+class ChatWindow(App):
+    def build(self):
+
+        # -------------- ALL SCREEN -------------- #
+        self.screen_manager = ScreenManager()
+
+        self.connect_page = ConnectPage()
+        screen = Screen(name="Connect")
+        screen.add_widget(self.connect_page)
+        self.screen_manager.add_widget(screen)
+
+        self.info_page = InfoPage()
+        screen = Screen(name="Info")
+        screen.add_widget(self.info_page)
+        self.screen_manager.add_widget(screen)
+
+        return self.screen_manager
+
+    def create_chat_page(self):
+        self.chat_page = ChatPage()
+        screen = Screen(name="Chat")
+        screen.add_widget(self.chat_page)
+        self.screen_manager.add_widget(screen)
+
+
+def show_error(message):
+    chat_app.info_page.update_info(message)
+    chat_app.screen_manager.current = "Info"
+    Clock.schedule_once(sys.exit, 10)
+
+if __name__ == "__main__":
+    chat_app = ChatWindow()
+    chat_app.run()
+
+
+
+# Ignore a value when unpacking
+# x, _, y = (1, 2, 3)  # x = 1, y = 3
+#  Ignore the multiple values. It is called "Extended Unpacking" which is available in only Python 3.x
+# x, *_, y = (1, 2, 3, 4, 5)  # x = 1, y = 5  # Ignore the index
--- server.py
+++ server.py
@@ -2,24 +2,7 @@ import socket
 import select
 import pickle
 
-
 HEADER_LENGTH = 10
-IP = "127.0.0.1"
-PORT = 1234
-
-# -------------------- CREATE SERVER SOCKET -------------------- #
-# AF_INET => IPv4
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-server_socket.bind((IP, PORT))
-server_socket.listen()
-
-sockets_list = [server_socket]  # All sockets
-clients_list = {}               # client sockets
-
-print(f'Listening for connections on {IP}:{PORT}...')
-
 
 # -------------------- RECEIVE MESSAGE -------------------- #
 def receive_message(client_socket):
@@ -39,52 +22,73 @@ def receive_message(client_socket):
     except:
         return False
 
-# -------------------- SERVER CONNECT TO CLIENT -------------------- #
+
+def creat_server_socket(IP, PORT):
+    # -------------------- CREATE SERVER SOCKET -------------------- #
+    # AF_INET => IPv4
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_socket.bind((IP, PORT))
+    server_socket.listen()
+
+    sockets_list = [server_socket]  # All sockets
+    clients_list = {}               # client sockets
+
+    print(f'Listening for connections on {IP}:{PORT}...')
+
+    # -------------------- SERVER CONNECT TO CLIENT -------------------- #
+
+    while True:
+
+        # select.select(read list, write list, excption list)
+        read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+
+        for notified_socket in read_sockets:
+
+            # ------------- NEW CLIENT CONNECTION ------------- #
+            if notified_socket == server_socket:
+                client_socket, client_address = server_socket.accept()
+                user = receive_message(client_socket, )
+                if user is False:
+                    continue
+
+                sockets_list.append(client_socket)
+                clients_list[client_socket] = user
+
+                print(f"Accept new connection from {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}")
+
+            # ------------- EXISTING CLIENT ------------- #
+            else:
+                message = receive_message(notified_socket)
+                #print(f"message: {message}")
+
+                # ---------- DISCONNECT ---------- #
+                if message is False:
+                    print(f"Closed connection from {clients_list[notified_socket]['data'].decode('utf-8')}")
+                    sockets_list.remove(notified_socket)
+                    del clients_list[notified_socket]
+                    continue
+
+                # ---------- RECEIVE MESSAGE ---------- #
+
+                user = clients_list[notified_socket]
+                print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+
+                for client_socket in clients_list:
+                    if client_socket != notified_socket:
+                        client_socket.send(user['header']+user['data']+message['header']+message['data'])
+
+        for notified_socket in exception_sockets:
+            sockets_list.remove(notified_socket)
+            del clients_list[notified_socket]
 
 
-while True:
+IP = "127.0.0.1"
+PORT = 1234
+creat_server_socket(IP,PORT)
 
-    # select.select(read list, write list, excption list)
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
-    for notified_socket in read_sockets:
-
-        # ------------- NEW CLIENT CONNECTION ------------- #
-        if notified_socket == server_socket:
-            client_socket, client_address = server_socket.accept()
-            user = receive_message(client_socket)
-            if user is False:
-                continue
-
-            sockets_list.append(client_socket)
-            clients_list[client_socket] = user
-
-            print(f"Accept new connection from {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}")
-
-        # ------------- EXISTING CLIENT ------------- #
-        else:
-            message = receive_message(notified_socket)
-            #print(f"message: {message}")
-
-            # ---------- DISCONNECT ---------- #
-            if message is False:
-                print(f"Closed connection from {clients_list[notified_socket]['data'].decode('utf-8')}")
-                sockets_list.remove(notified_socket)
-                del clients_list[notified_socket]
-                continue
-
-            # ---------- RECEIVE MESSAGE ---------- #
-
-            user = clients_list[notified_socket]
-            print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
-
-            for client_socket in clients_list:
-                if client_socket != notified_socket:
-                    client_socket.send(user['header']+user['data']+message['header']+message['data'])
-
-    for notified_socket in exception_sockets:
-        sockets_list.remove(notified_socket)
-        del clients_list[notified_socket]
 
 # socket.error: [Errno 48] Address already in use:
 # ps -fA | grep python
