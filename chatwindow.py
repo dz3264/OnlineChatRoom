5c026bd0998e0435965b2875b560021516f7edef
HuaxinZhang <dianazhang0219@gmail.com>
2020-01-27 00:54:16 -0700

    function

--- chatwindow.py
+++ chatwindow.py
@@ -1,6 +1,6 @@
 import os
 import sys
-#import server
+# import server
 import client
 from kivy.app import App
 from kivy.uix.label import Label
@@ -9,6 +9,34 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
+from functools import partial
+
+
+# ----------------- AUTO SCROLL WHEN NEW MESSAGE ----------------- #
+class ScrollLabel(ScrollView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.layout = GridLayout(cols = 1, size_hint_y=None)
+        self.add_widget(self.layout)
+
+        self.chat_history = Label(size_hint_y=None, markup=True)
+        self.scroll_to_point = Label()
+
+        self.layout.add_widget(self.chat_history)
+        self.layout.add_widget(self.scroll_to_point)
+
+    def update_chat_history(self, message):
+
+        self.chat_history.text += '\n' + message
+
+        self.layout.height = self.chat_history.texture_size[1] + 15
+        self.chat_history.height = self.chat_history.texture_size[1]
+        self.chat_history.text_size = (self.chat_history.width * 0.98, None)
+
+        self.scroll_to(self.scroll_to_point)
+
 
 # ----------------- SIGN IN PAGE ----------------- #
 class ConnectPage(GridLayout):
@@ -29,15 +57,15 @@ class ConnectPage(GridLayout):
             prev_username = ""
 
         self.add_widget(Label(text="IP: "))
-        self.ip = TextInput(text = prev_ip, multiline=False)
+        self.ip = TextInput(text=prev_ip, multiline=False)
         self.add_widget(self.ip)
 
         self.add_widget(Label(text="PORT: "))
-        self.port = TextInput(text = prev_port, multiline=False)
+        self.port = TextInput(text=prev_port, multiline=False)
         self.add_widget(self.port)
 
-        self.add_widget(Label(text = "Username: "))
-        self.username = TextInput(text = prev_username, multiline = False)
+        self.add_widget(Label(text="Username: "))
+        self.username = TextInput(text=prev_username, multiline=False)
         self.add_widget(self.username)
 
         self.join = Button(text="Join")
@@ -65,15 +93,16 @@ class ConnectPage(GridLayout):
         ip = self.ip.text
         username = self.username.text
 
-        if not client.create_client_socket(ip, port, username, show_error):
+        chat_app.my_client = client.Client(ip, port, username, show_error)
+
+        # print(chat_app.my_client.client_socket)
+        if not chat_app.my_client.client_socket:
             return
 
-        print("CHANGE TO CHAT")
         # since chat page listen to connection
         # so we have to create chatpage after connection
         chat_app.create_chat_page()
         chat_app.screen_manager.current = "Chat"
-
 
 
 # ----------------- INFO PAGE ----------------- #
@@ -82,14 +111,14 @@ class InfoPage(GridLayout):
         super(InfoPage, self).__init__(**kwargs)
         self.cols = 1
         self.message = Label(halign="center", valign="middle", font_size=30)
-        self.message.bind(width= self.update_text_width)
+        self.message.bind(width=self.update_text_width)
         self.add_widget(self.message)
 
     def update_info(self, message):
         self.message.text = message
 
     def update_text_width(self, *_):
-        self.message.text_size = (self.message.width*0.9, None)
+        self.message.text_size = (self.message.width * 0.9, None)
 
 
 # ----------------- CHAT PAGE ----------------- #
@@ -97,28 +126,87 @@ class ChatPage(GridLayout):
     def __init__(self, **kwargs):
         super(ChatPage, self).__init__(**kwargs)
         self.cols = 1
-        self.add_widget(Label(text = "Begin Chat !!"))
+        self.rows = 2
+
+        # -------------- FIRST ROW -------------- #
+
+        # Window.size[0] is x, Window.size[1] is y
+        self.history = ScrollLabel(height=Window.size[1] * 0.8, size_hint_y=None)
+        self.add_widget(self.history)
+
+        # -------------- SECOND ROW -------------- #
+        self.new_message = TextInput(width=Window.size[0] * 0.8, size_hint_x=None, multiline=False)
+        self.send = Button(text = "Send")
+        self.send.bind(on_press=self.send_message)
+
+        lower_page = GridLayout(cols = 2)
+        lower_page.add_widget(self.new_message)
+        lower_page.add_widget(self.send)
+        self.add_widget(lower_page)
+
+        # bind enter with send
+        Window.bind(on_key_down = self.on_key_down)
+
+        # check input per 1 sec
+        Clock.schedule_once(self.focus_text_input, 1)
+        Clock.schedule_interval(self.receive_message, 1)
+        #chat_app.my_client.client_listening(self.incoming_message, show_error)
+
+    def on_key_down(self, instance, keyboard, keycode, text, modifiers):
+        # Enter key = 40
+        if keyboard == 40:
+            self.send_message(None)
+
+    def receive_message(self, _):
+        chat_app.my_client.client_listening(self.incoming_message, show_error)
+
+    def send_message(self, _):
+
+        # get the input message
+        message = self.new_message.text
+        chat_app.my_client.client_listening(self.incoming_message, show_error)
+
+        # clean the input box
+        self.new_message.text = ""
+
+        if message:
+            self.history.update_chat_history(f'[color=fcc3c3]{chat_app.connect_page.username.text}[/color] > {message}')
+            chat_app.my_client.client_send(message)
+
+        Clock.schedule_once(self.focus_text_input, 0.1)
+
+    def focus_text_input(self,_):
+        self.new_message.focus = True
+
+
+    def incoming_message(self, username, message):
+        self.history.update_chat_history(f"[color=bee3f5]{username}[/color] > {message}")
+
 
 
 class ChatWindow(App):
     def build(self):
-
         # -------------- ALL SCREEN -------------- #
         self.screen_manager = ScreenManager()
 
+        # -------------- CONNECT PAGE -------------- #
         self.connect_page = ConnectPage()
         screen = Screen(name="Connect")
         screen.add_widget(self.connect_page)
         self.screen_manager.add_widget(screen)
 
+        # -------------- IFNO PAGE -------------- #
         self.info_page = InfoPage()
         screen = Screen(name="Info")
         screen.add_widget(self.info_page)
         self.screen_manager.add_widget(screen)
 
+        self.my_client = None
+
         return self.screen_manager
 
     def create_chat_page(self):
+        # -------------- CHAT PAGE -------------- #
         self.chat_page = ChatPage()
         screen = Screen(name="Chat")
         screen.add_widget(self.chat_page)
@@ -130,11 +218,10 @@ def show_error(message):
     chat_app.screen_manager.current = "Info"
     Clock.schedule_once(sys.exit, 10)
 
+
 if __name__ == "__main__":
     chat_app = ChatWindow()
     chat_app.run()
-
-
 
 # Ignore a value when unpacking
 # x, _, y = (1, 2, 3)  # x = 1, y = 3
--- client.py
+++ client.py
@@ -6,74 +6,83 @@ import sys
 
 HEADER_LENGTH = 10
 
+class Client():
 
-# -------------------- CREATE CLIENT SOCKET -------------------- #
-def create_client_socket(IP, PORT, my_username, show_error):
-    # my_username = input("Username: ")
+    def __init__(self, ip, port, username, show_error):
 
-    try:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((IP, PORT))
-        client_socket.setblocking(False)
-
-        username = my_username.encode('utf-8')
-        username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-        client_socket.send(username_header + username)
-
-        return client_socket
-
-    except Exception as e:
-        show_error(str(e))
-        # sys.exit()
-
-# -------------------- MESSAGE COMMUNICATION -------------------- #
-def client_communication(my_username, client_socket, show_error):
-    while True:
-
-        # ----------------- SEND MESSAGE ----------------- #
-        message = input(f'{my_username} > ')
-        # Automatic reader
-        # message = ""
-
-        if message:
-            message = message.encode('utf-8')
-            message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-            client_socket.send(message_header + message)
-
-        # ----------------- RECEIVE MESSAGE ----------------- #
         try:
-            while True:
-                username_header = client_socket.recv(HEADER_LENGTH)
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((ip, port))
+            client_socket.setblocking(False)
 
-                if not len(username_header):
-                    print("Connection closed by the server")
-                    sys.exit()
+            username = username.encode('utf-8')
+            username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+            client_socket.send(username_header + username)
 
-                username_length = int(username_header.decode('utf-8').strip())
-                username = client_socket.recv(username_length).decode('utf-8')
+            self.client_socket = client_socket
 
-                message_header = client_socket.recv(HEADER_LENGTH)
-                message_length = int(message_header.decode('utf-8').strip())
-                message = client_socket.recv(message_length).decode('utf-8')
+        except Exception as e:
+            self.client_socket = None
+            show_error(str(e))
+            # sys.exit()
 
-                print(f"{username} > {message}")
+    def client_listening(self, incoming_message, show_error):
+
+        try:
+            username_header = self.client_socket.recv(HEADER_LENGTH)
+
+            if not len(username_header):
+                print("Connection closed by the server")
+                sys.exit()
+
+            username_length = int(username_header.decode('utf-8').strip())
+            username = self.client_socket.recv(username_length).decode('utf-8')
+
+            message_header = self.client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+            message = self.client_socket.recv(message_length).decode('utf-8')
+
+            incoming_message(username, message)
 
 
         except IOError as e:
             if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
                 print('Reading error: ', str(e))
-                #show_error(str(e))
-                #sys.exit()
-            continue
+                show_error(str(e))
+                # sys.exit()
 
         except Exception as e:
 
             print("General error: ", str(e))
-            #show_error(str(e))
-            #sys.exit()
+            show_error(str(e))
+            # sys.exit()
 
 
-#IP = "127.0.0.1"
-#PORT = 1234
-#my_username = 'test'
-#create_client_socket(IP, PORT, my_username)
+    # -------------------- MESSAGE COMMUNICATION -------------------- #
+    def client_send(self, message):
+
+        # ----------------- SEND MESSAGE ----------------- #
+        # message = input(' >> ')
+        # Automatic reader
+        # message = ""
+        if message and isinstance(message, str):
+            message = message.encode('utf-8')
+            message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+            self.client_socket.send(message_header + message)
+
+
+"""
+IP = "127.0.0.1"
+PORT = 1234
+my_username = 'Client Diana'
+
+def show_error():
+    return 1
+
+def incomming_message(username, message):
+    print(f"{username} > {message}")
+
+my_client = Client(IP, PORT, my_username, show_error)
+my_client.client_listening(incomming_message, show_error)
+my_client.client_send("my message")
+"""
